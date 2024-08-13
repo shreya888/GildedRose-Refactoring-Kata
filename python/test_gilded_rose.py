a117545873d490a2be728bcc70e4616d0b79a033
Shreya Chawla <shreya.chawla@anu.edu.au>
2024-08-14 04:20:27 +1000

    Refractored code and test with correct names

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -11,11 +11,11 @@ class GildedRoseTest(unittest.TestCase):
         self.assert_item_values(Item("foo", 1, 0), 0)
         self.assert_item_values(Item("Aged Brie", 10, 4), 5)
         self.assert_item_values(Item("Aged Brie", 10, 50), 50)
-        self.assert_sulfuras_values(Item("Sulfuras, Hand of Ragnaros", 10, 40))
-        self.assert_sulfuras_values(Item("Sulfuras, Hand of Ragnaros", 10, 80))
-        self.assert_item_values(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10), 12)
-        self.assert_item_values(Item("Backstage passes to a TAFKAL80ETC concert", 5, 10), 13)
-        self.assert_item_values(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10), 0)
+        self.assert_sulfuras_values(Item("Sulfuras", 10, 40))
+        self.assert_sulfuras_values(Item("Sulfuras", 10, 80))
+        self.assert_item_values(Item("Backstage passes", 10, 10), 12)
+        self.assert_item_values(Item("Backstage passes", 5, 10), 13)
+        self.assert_item_values(Item("Backstage passes", 0, 10), 0)
 
     def assert_item_values(self, item, expected_quality):
         expected_sell_in = item.sell_in - 1
@@ -27,7 +27,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(expected_quality, item.quality)
 
     def assert_sulfuras_values(self, item):
-        self.assertEqual(item.name, "Sulfuras, Hand of Ragnaros")
+        self.assertEqual(item.name, "Sulfuras")
 
         gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
