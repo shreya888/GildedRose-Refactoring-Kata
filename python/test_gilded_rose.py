cc4aae78603086b529f3b5054622c5d251607d40
Shreya Chawla <shreya.chawla@anu.edu.au>
2024-08-14 04:22:59 +1000

    Refractored tests to be more readable and reusable according to DRY principle

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,36 +5,32 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_update_item(self):
-        self.assert_item_values(Item("foo", 10, 10), 9)
-        self.assert_item_values(Item("foo", 0, 10), 8)
-        self.assert_item_values(Item("foo", 1, 0), 0)
-        self.assert_item_values(Item("Aged Brie", 10, 4), 5)
-        self.assert_item_values(Item("Aged Brie", 10, 50), 50)
-        self.assert_sulfuras_values(Item("Sulfuras", 10, 40))
-        self.assert_sulfuras_values(Item("Sulfuras", 10, 80))
-        self.assert_item_values(Item("Backstage passes", 10, 10), 12)
-        self.assert_item_values(Item("Backstage passes", 5, 10), 13)
-        self.assert_item_values(Item("Backstage passes", 0, 10), 0)
+    def test_update_standard_item(self):
+        """Test to check update for standard items."""
+        self.assert_item_values(Item("foo", 10, 10), 9, 9)
+        self.assert_item_values(Item("foo", 0, 10), -1, 8)
+        self.assert_item_values(Item("foo", 1, 0), 0, 0)
 
-    def assert_item_values(self, item, expected_quality):
-        expected_sell_in = item.sell_in - 1
+    def test_update_aged_brie(self):
+        """Test to check update for Aged Brie items."""
+        self.assert_item_values(Item("Aged Brie", 10, 4), 9, 5)
+        self.assert_item_values(Item("Aged Brie", 10, 50), 9, 50)
 
+    def test_update_sulfuras(self):
+        """Test to check update for Sulfuras items."""
+        self.assert_item_values(Item("Sulfuras", 10, 40), 10, 40)
+        self.assert_item_values(Item("Sulfuras", 10, 80), 10, 80)
+
+    def test_update_backstage_passes(self):
+        """Test to check update for Backstage passes."""
+        self.assert_item_values(Item("Backstage passes", 10, 10), 9, 12)
+        self.assert_item_values(Item("Backstage passes", 5, 10), 4, 13)
+        self.assert_item_values(Item("Backstage passes", 0, 10), -1, 0)
+
+    def assert_item_values(self, item, expected_sell_in, expected_quality):
+        """Assert statements to match the expected sell_in and quality values after updating the item."""
         gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
-
-        self.assertEqual(expected_sell_in, item.sell_in)
-        self.assertEqual(expected_quality, item.quality)
-
-    def assert_sulfuras_values(self, item):
-        self.assertEqual(item.name, "Sulfuras")
-
-        gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
-
-        expected_sell_in = item.sell_in
-        expected_quality = item.quality
-
+        gilded_rose.update()
         self.assertEqual(expected_sell_in, item.sell_in)
         self.assertEqual(expected_quality, item.quality)
 
